f1d96bfe734f2a736f1faf423c0017d96dc26f86
velkob <velko.bonev@abv.bg>
2021-03-17 00:36:20 +0200

    Added animations for walking and jumping

---
+++ secondHomework/SecondHomework/Assets/Scripts/Jumping.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + vertical * Time.deltaTime; 
+    }
+}
