cc4c88415e72d87f888b21e39570ec5e3afd7d7b
velkob <43839405+velkob@users.noreply.github.com>
2021-05-25 13:48:39 +0300

    Added better material for the ground and changed roll button pos

---
+++ CityTycoon/City Tycoon/Assets/Scripts/UI/Money.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Money : MonoBehaviour
+{
+    private int money;
+
+    [SerializeField]
+    private GameObject player;
+
+    private void Update()
+    {
+        gameObject.GetComponent<TextMeshProUGUI>().text = player.GetComponent<PlayerInfo>().getMoney().ToString() + "$";
+    }
+}
