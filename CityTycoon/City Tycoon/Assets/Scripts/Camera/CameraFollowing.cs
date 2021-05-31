26c7dc10827863b7f07a1293d6fb7f79d8ce91a2
velkob <43839405+velkob@users.noreply.github.com>
2021-05-31 14:06:56 +0300

    Made camera rotatable

--- CityTycoon/City Tycoon/Assets/Scripts/Camera/CameraFollowing.cs
+++ CityTycoon/City Tycoon/Assets/Scripts/Camera/CameraFollowing.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class CameraFollowing : MonoBehaviour
 {
-    [SerializeField]
-    private Transform player;
-    [SerializeField]
-    private Vector3 rotation;
-    [SerializeField]
+    //    [SerializeField]
+    //    private Transform player;
+    //    [SerializeField]
+    //    private Vector3 rotation;
+    //    [SerializeField]
+    //    private Vector3 offset;
+
+    //    private void Start()
+    //    {
+    //        transform.eulerAngles = rotation;
+    //    }
+    //    void LateUpdate()
+    //    {
+    //        transform.position = player.position + offset;
+    //        transform.eulerAngles = player.rotation.eulerAngles + rotation;
+    //    }
+
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
     private Vector3 offset;
 
-    private void Start()
+    void Start()
     {
-        transform.eulerAngles = rotation;
+        offset = new Vector3(player.position.x, player.position.y + 45.0f, player.position.z + 7.0f);
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
     }
+
     void LateUpdate()
     {
         transform.position = player.position + offset;
+        if (Input.GetMouseButton(1))
+        {
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            transform.LookAt(player.position);
+        }
     }
 }
